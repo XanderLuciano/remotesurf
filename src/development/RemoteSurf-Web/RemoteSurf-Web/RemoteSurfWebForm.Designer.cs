1f9faba3b6cf01d4f657d1b1b0a22bc6a3a0ccdc
XanderLuciano <vipercodegames@gmail.com>
2016-09-02 16:30:35 -0700

    Added a standalone-web program for creating an user interface with HTML templates.

---
+++ src/development/RemoteSurf-Web/RemoteSurf-Web/RemoteSurfWebForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace RemoteSurf
+{
+    partial class RemoteSurfWebForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RemoteSurfWebForm));
+            this.menustrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fullscreenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chromeDevToolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menustrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menustrip
+            // 
+            this.menustrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.fullscreenToolStripMenuItem});
+            this.menustrip.Location = new System.Drawing.Point(0, 0);
+            this.menustrip.Name = "menustrip";
+            this.menustrip.Size = new System.Drawing.Size(1264, 24);
+            this.menustrip.TabIndex = 0;
+            this.menustrip.Text = "RemoteSurf Menu";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.chromeDevToolsToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.openToolStripMenuItem.Text = "Open...";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenDevTools);
+            // 
+            // fullscreenToolStripMenuItem
+            // 
+            this.fullscreenToolStripMenuItem.Name = "fullscreenToolStripMenuItem";
+            this.fullscreenToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.fullscreenToolStripMenuItem.Text = "Fullscreen";
+            this.fullscreenToolStripMenuItem.Click += new System.EventHandler(this.ToggleFullScreen);
+            // 
+            // chromeDevToolsToolStripMenuItem
+            // 
+            this.chromeDevToolsToolStripMenuItem.Name = "chromeDevToolsToolStripMenuItem";
+            this.chromeDevToolsToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.chromeDevToolsToolStripMenuItem.Text = "Chrome Dev Tools";
+            this.chromeDevToolsToolStripMenuItem.Click += new System.EventHandler(this.OpenDevTools);
+            // 
+            // RemoteSurfWebForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 761);
+            this.Controls.Add(this.menustrip);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "RemoteSurfWebForm";
+            this.Text = "RemoteSurf Web Template";
+            this.menustrip.ResumeLayout(false);
+            this.menustrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menustrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fullscreenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem chromeDevToolsToolStripMenuItem;
+    }
+}
+
---
+++ src/development/RemoteSurf-Web/RemoteSurf-Web/RemoteSurfWebForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace RemoteSurf
+{
+    public partial class RemoteSurfWebForm : Form
+    {
+        public RemoteSurfWebForm()
+        {
+            InitializeComponent();
+
+            InitBrowser();
+        }
+        public bool fullscreen = false;
+        public ChromiumWebBrowser browser;
+
+        public void InitBrowser()
+        {
+            Cef.Initialize(new CefSettings());
+            browser = new ChromiumWebBrowser(@"file:///resources/html/index.html");
+            this.Controls.Add(browser);
+            browser.BringToFront();
+            browser.Dock = DockStyle.Fill;
+
+            browser.RegisterJsObject("winform", new jsInteraction(this));
+        }
+
+        private void ToggleFullScreen(object sender, EventArgs e)
+        {
+            if (!fullscreen)
+            {
+                this.TopMost = true;
+                this.FormBorderStyle = FormBorderStyle.None;
+                this.WindowState = FormWindowState.Maximized;
+            }
+            else
+            {
+                this.TopMost = false;
+                this.FormBorderStyle = FormBorderStyle.Sizable;
+                this.WindowState = FormWindowState.Normal;
+            }
+            fullscreen = !fullscreen;
+        }
+
+        private void OpenDevTools(object sender, EventArgs e)
+        {
+            browser.ShowDevTools();
+        }
+    }
+}
