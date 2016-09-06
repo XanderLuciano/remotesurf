04c00852bf8fc493f615b356c3cffc4fdab37409
XanderLuciano <vipercodegames@gmail.com>
2016-09-06 10:53:20 -0700

    fixed file tree

---
+++ src/standalone-web/RemoteSurf-Web/RemoteSurfWebForm.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace RemoteSurf
+{
+    partial class RemoteSurfWebForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RemoteSurfWebForm));
+            this.SuspendLayout();
+            // 
+            // RemoteSurfWebForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1084, 641);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "RemoteSurfWebForm";
+            this.Text = "RemoteSurf Web Template";
+            this.Resize += new System.EventHandler(this.Resized);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ src/standalone-web/RemoteSurf-Web/jsInteraction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RemoteSurf
+{
+    public class jsInteraction
+    {
+        public bool fullscreen = false;
+
+        private Form form;
+
+        public jsInteraction(Form remoteSurf)
+        {
+            form = remoteSurf;
+        }
+
+        public void toggleFullScreen()
+        {
+            fullscreen = !fullscreen;
+            if (form.InvokeRequired)
+            {
+                form.Invoke((MethodInvoker)delegate
+                {
+                    form.TopMost = fullscreen;
+                    form.FormBorderStyle = (fullscreen) ? FormBorderStyle.None : FormBorderStyle.Sizable;
+                    form.WindowState = (fullscreen) ? FormWindowState.Maximized : FormWindowState.Normal;
+                });
+            }
+            else
+            {
+                form.TopMost = fullscreen;
+                form.FormBorderStyle = (fullscreen) ? FormBorderStyle.None : FormBorderStyle.Sizable;
+                form.WindowState = (fullscreen) ? FormWindowState.Maximized : FormWindowState.Normal;
+            }
+        }
+    }
+}
